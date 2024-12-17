8c35e2a294e28151d7790c73f1235d6307ec7d41
RomashkaDeveloper <164900318+RomashkaDeveloper@users.noreply.github.com>
2024-12-17 11:08:19 +0300

    Create setup-v1.py

---
+++ setup-v1.py
@@ -0,0 +1,36 @@
+while True:
+    print('Choose your method:\n1.transormers\2.unsloth')
+    user_method_id = input('—> ')
+
+    if user_metbreakhod_is == 1:
+        print('choose your model\n1.gg\n2.hh\3.kk\n4. custom model')
+        user_model_id = input('—> ')
+        if user_model_id == 1:
+            model = ””
+            break
+        elif user_model_id == 2:
+            model = ""
+            break
+        elif user_model_id == 3:
+            model = ""
+            break
+        else:
+            model = input('Type your model’s name here: ')
+            break
+    elif user_method_is == 2:
+        print('choose your model\n1.gg\n2.hh\n3.ll\n4. custom model')
+        user_model_id = input('—> ')
+        if user_model_id == 1:
+            model = ””
+            break
+        elif user_model_id == 2:
+            model = ""
+            break
+        elif user_model_id == 3:
+            model = ""
+            break
+        else:
+            model = input('Type your model’s name here: ')
+            break
+    else:
+        print('ты дегенерат')
