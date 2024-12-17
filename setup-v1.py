5d9f55baf51c818364da3023527a23169457f89d
RomashkaDeveloper <164900318+RomashkaDeveloper@users.noreply.github.com>
2024-12-17 11:26:46 +0300

    Update setup-v1.py

--- setup-v1.py
+++ setup-v1.py
@@ -1,3 +1,5 @@
+
+import configparser
 while True:
     print('Choose your method:\n1.transormers\2.unsloth')
     user_method_id = input('—> ')
@@ -6,31 +8,32 @@ while True:
         print('choose your model\n1.gg\n2.hh\3.kk\n4. custom model')
         user_model_id = input('—> ')
         if user_model_id == 1:
-            model = ””
-            break
+            model = ""
         elif user_model_id == 2:
             model = ""
-            break
         elif user_model_id == 3:
             model = ""
-            break
         else:
             model = input('Type your model’s name here: ')
-            break
+        method = "transformers"
+        break
     elif user_method_is == 2:
         print('choose your model\n1.gg\n2.hh\n3.ll\n4. custom model')
         user_model_id = input('—> ')
         if user_model_id == 1:
-            model = ””
-            break
+            model = ""
         elif user_model_id == 2:
             model = ""
-            break
         elif user_model_id == 3:
             model = ""
-            break
         else:
             model = input('Type your model’s name here: ')
-            break
+        method = "unsloth"
+        break
     else:
         print('ты дегенерат')
+config = configparser.ConfigParser()
+config['SYSTEM']['model'] = model
+config['SYSTEM']['method'] = method
+with open('config.ini', 'w') as configfile:
+    config.write(configfile)
